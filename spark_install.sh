77239cfb624c26fbb4b4f2955bc276cdcb9a3a26
Archisman Roy <>
2021-04-24 07:43:14 +0000

    added maven download commands

--- spark_install.sh
+++ spark_install.sh
@@ -1,5 +1,15 @@
 echo "start spark install"
 wget https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz
 tar xvf spark-3.1.1-bin-hadoop3.2.tgz
-mv spark-3.1.1-bin-hadoop3.2.tgz spark
+mv spark-3.1.1-bin-hadoop3.2 spark
 mv /home/runner/java-spark/spark /home/runner/
+rm spark-3.1.1-bin-hadoop3.2.tgz
+
+
+echo "start maven update"
+wget https://downloads.apache.org/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.tar.gz
+tar xzvf apache-maven-3.8.1-bin.tar.gz
+mv apache-maven-3.8.1 maven 
+mv maven /home/runner/
+rm apache-maven-3.8.1-bin.tar.gz
+export PATH="/home/runner/maven:$PATH"
