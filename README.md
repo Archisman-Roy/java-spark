4e3b80e47b5d61d0ac6dbe1ae10559b4c63516bb
Archisman Roy <>
2021-04-26 05:12:28 +0000

    Working spark-submit

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Run the following in shell to setup spark:
 chmod +x spark_install.sh
 ./spark_install.sh
 ```
+
+Run the following in shell to submit a spark job:
+```shell
+../spark/bin/spark-submit --class "roy.javaspark.JavaSpark" --master local[4] target/javaspark-1.0-SNAPSHOT.jar
+  ```
--- src/main/java/roy/javaspark/JavaSpark.java
+++ src/main/java/roy/javaspark/JavaSpark.java
@@ -1,10 +1,11 @@
-/* mainApp.java */
+package roy.javaspark;
+
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 
-public class MainApp {
+public class JavaSpark {
   public static void main(String[] args) {
-    String logFile = ""; // Should be some file on your system
+    
     SparkSession spark = SparkSession.builder().appName("nyc-trip-processing").getOrCreate();
     
     System.out.println("Test worked");
