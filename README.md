ad8fe662f47778a8ae6345fc2dcf463fef2a9fbf
Archisman-Roy <royarchi31@gmail.com>
2021-06-30 04:16:56 +0000

    #

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 # Learning Java and Spark
-## Setup instructions
-
-Run the following in shell to setup spark:
-```shell 
-chmod +x spark_install.sh
-./spark_install.sh
-```
-
-Run the following in shell to submit a spark job:
-```shell
-../spark/bin/spark-submit --class "roy.javaspark.JavaSpark" --master local[4] target/javaspark-1.0-SNAPSHOT.jar
-  ```
--- spark_install.sh
+++ spark_install.sh
@@ -1,9 +1,9 @@
-echo "start spark install"
-wget https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz
-tar xvf spark-3.1.1-bin-hadoop3.2.tgz
-mv spark-3.1.1-bin-hadoop3.2 spark
-mv /home/runner/java-spark/spark /home/runner/
-rm spark-3.1.1-bin-hadoop3.2.tgz
+#echo "start spark install"
+#wget https://downloads.apache.org/spark/spark-3.1.1/spark-3.1.1-bin-hadoop3.2.tgz
+#tar xvf spark-3.1.1-bin-hadoop3.2.tgz
+#mv spark-3.1.1-bin-hadoop3.2 spark
+#mv /home/runner/java-spark/spark /home/runner/
+#rm spark-3.1.1-bin-hadoop3.2.tgz
 
 
 #echo "start maven update"
@@ -13,3 +13,6 @@ rm spark-3.1.1-bin-hadoop3.2.tgz
 #mv maven /home/runner/
 #rm apache-maven-3.8.1-bin.tar.gz
 #export PATH="/home/runner/maven:$PATH"
+
+#git config --global user.name "Archisman-Roy"
+#git config --global user.email johndoe@example.com
